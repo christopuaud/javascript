d4f907eb55d2ebf78418f4e0c2efec35d2ff248d
koalanoir <ngdorian0@gmail.com>
2023-02-15 16:51:55 +0100

    ci start

--- game.spec.js
+++ game.spec.js
@@ -3,6 +3,8 @@ require('./game.js');
 describe("The test environment", function() {
   it("should pass", function() {
     expect(true).toBe(false);
+
+    
   });
 
   it("should access game", function() {
